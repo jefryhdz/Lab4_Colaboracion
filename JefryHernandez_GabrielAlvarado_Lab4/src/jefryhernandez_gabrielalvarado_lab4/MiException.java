df4f6284757dca66dd45ae2f98d22aa08316a7fd
Gabriel Alvarado <gabriel_alvararado24@hotmail.com>
2017-08-11 17:05:01 -0600

    Hice la exception

---
+++ JefryHernandez_GabrielAlvarado_Lab4/src/jefryhernandez_gabrielalvarado_lab4/MiException.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jefryhernandez_gabrielalvarado_lab4;
+
+import java.awt.Color;
+
+/**
+ *
+ * @author Gabriel Alvarado
+ */
+public class MiException extends Exception{
+
+    public MiException(String string) {
+        super(string);
+    }
+    
+    
+}
