008444deaabdd30cf0435bb9e40bd19088701881
jefryhdz <jefryhdz@gmail.com>
2017-08-11 13:47:59 -0600

    primeros cambios

---
+++ JefryHernandez_GabrielAlvarado_Lab4/src/jefryhernandez_gabrielalvarado_lab4/Movimiento.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jefryhernandez_gabrielalvarado_lab4;
+
+/**
+ *
+ * @author Jefry Hernandez
+ */
+public interface Movimiento {
+    public void movimiento();
+
+}
---
+++ JefryHernandez_GabrielAlvarado_Lab4/src/jefryhernandez_gabrielalvarado_lab4/Pieza.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jefryhernandez_gabrielalvarado_lab4;
+
+import java.awt.Color;
+
+/**
+ *
+ * @author Jefry Hernandez
+ */
+public abstract class Pieza implements Movimiento {
+
+    protected Color color;
+
+    public Pieza(Color color) {
+        this.color = color;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    @Override
+    public String toString() {
+        return "Pieza{" + "color=" + color + '}';
+    }
+
+}
